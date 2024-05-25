b37b15da461ca5611ac737bb9fe5e0a54ae9fd7d
Jas9991 <62826563+Jas9991@users.noreply.github.com>
2024-05-25 13:14:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -142,6 +142,8 @@ app.get("/posts/:postId", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server is up and running on port 3000.");
 });
